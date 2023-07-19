e365bcad57ae01c80a1af4c888871fe6f1e46551
onebottlekick <gksqudcks97@gmail.com>
2023-07-19 12:24:00 +0900

    added functions

--- plot.py
+++ plot.py
@@ -19,6 +19,26 @@ plot_dqn_conv_cart_positions = partial(plot_fig, kind='cart_positions', algo='DQ
 plot_dqn_conv_pole_angles = partial(plot_fig, kind='pole_angles', algo='DQN_Conv', save_fig=True)
 plot_dqn_conv_rewards = partial(plot_fig, kind='episode_rewards', algo='DQN_Conv', save_fig=True)
 
+def SARSA():
+    plot_sarsa_cart_positions()
+    plot_sarsa_pole_angles()
+    plot_sarsa_rewards()
+    
+def Q_Learning():
+    plot_q_learning_cart_positions()
+    plot_q_learning_pole_angles()
+    plot_q_learning_rewards()
+    
+def DQN_Linear():
+    plot_dqn_linear_cart_positions()
+    plot_dqn_linear_pole_angles()
+    plot_dqn_linear_rewards()
+    
+def DQN_Conv():
+    plot_dqn_conv_cart_positions()
+    plot_dqn_conv_pole_angles()
+    plot_dqn_conv_rewards()
+
 
 if __name__ == '__main__':
-    plot_dqn_conv_pole_angles()
+    DQN_Linear()
